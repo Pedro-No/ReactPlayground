2ef6dfa3b3ae573f540d291652f76bc206128638
Pedro-No <pedr0.nogueira@hotmail.com>
2025-09-13 15:42:15 +0100

    Created Base Application and it's first test component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import UploadButton from "./components/upload-button/upload-button";
+import UploadImageResizer from "./components/upload-image/upload-image-resizer";
+
+export default function App() {
+  const [imageFile, setImageFile] = useState(null);
+
+  return (
+    <>
+      <h1>React Playground</h1>
+      <UploadButton onImageSelected={setImageFile} />
+      {imageFile && <UploadImageResizer imageFile={imageFile} />}
+    </>
+  );
+}
